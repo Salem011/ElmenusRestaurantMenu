2257134f014a0de636788fc0e0141cb6b0b78ac3
Salem Mohammed <salem.muhmed@gmail.com>
2018-04-27 19:01:10 +0200

    add tableviewheader

---
+++ ElmenusResturantMenuTask/Views/MenuTableHeaderView.swift
@@ -0,0 +1,16 @@
+//
+//  MenuTableHeaderView.swift
+//  ElmenusResturantMenuTask
+//
+//  Created by Salem Mohamed on 4/27/18.
+//  Copyright © 2018 Salem Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableHeaderView: UIView {
+
+    @IBOutlet weak var headerTitleLabel: UILabel!
+    
+    
+}
