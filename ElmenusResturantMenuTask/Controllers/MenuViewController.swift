d8fd0b6eba15d2ee3a6150feea7b9f024b6d9be1
Salem Mohammed <salem.muhmed@gmail.com>
2018-04-27 17:11:15 +0200

    create menuVC

---
+++ ElmenusResturantMenuTask/Controllers/MenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MenuViewController.swift
+//  ElmenusResturantMenuTask
+//
+//  Created by Salem Mohamed on 4/27/18.
+//  Copyright © 2018 Salem Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
